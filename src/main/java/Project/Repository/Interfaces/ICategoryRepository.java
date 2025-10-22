b70fb1681efab665b318592274164f93eebded1a
Marouane-bichara <bicharamarouane0@gmail.com>
2025-10-22 12:29:14 +0200

    finished repository category

--- src/main/java/Project/Repository/Interfaces/ICategoryRepository.java
+++ src/main/java/Project/Repository/Interfaces/ICategoryRepository.java
@@ -1,4 +1,10 @@
 package Project.Repository.Interfaces;
 
-public interface ICategoryRepository {
+import Project.Entities.Category;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ICategoryRepository extends JpaRepository<Category, Long>{
 }
